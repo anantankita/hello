654a97dc9bb183c38c0b3f0e5b16b87677b97ef7
anantankita <40429566+anantankita@users.noreply.github.com>
2018-10-21 22:30:44 +0530

    just some refactoring

--- src/App.js
+++ src/App.js
@@ -2,17 +2,8 @@ import React, { Component } from 'react';
 import Profile from './components/Profile'
 
 class App extends Component {
-  // constructor(){
-  //   super();
-  //
-  //   this.state = {
-  //     profile: ['Name:', 'Address:', 'Email:', 'Phone:']
-  //   }
-  // }
 
   render() {
-
-    // const {profile} = this.state;
 
     return (
       <div className="App">
